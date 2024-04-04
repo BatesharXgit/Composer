0c3dd319147e2798b95d3dab96f5b2e620168963
BatesharXgit <bateshar.xyz@gmail.com>
2024-04-04 22:57:36 +0530

    composers data from server

---
+++ composer/lib/data/composer_data/focus_composer.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+
+final List<String> natureaudioPaths = [
+  //nature
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Homepage%2FRecommended%2FZen%20Garden%20Music.mp3?alt=media&token=fd4f17fa-b0d0-4795-9cba-5e9fdd2a7c41',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FNature%2Fmystic-forest-ambient-23812.mp3?alt=media&token=8d1481d3-f57a-4828-96a3-4894686b9d89',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FNature%2Flava-loop-3-28887.mp3?alt=media&token=6d6a36bb-fc27-4f77-bf30-f43e560616c1',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FNature%2Fstream-river-water-up-close-6061.mp3?alt=media&token=9b0877f7-2c11-494b-90dc-e2be3c104600',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FNature%2Frelax-in-the-forest-background-music-for-video-9145.mp3?alt=media&token=5d5ba687-1423-4abc-98f2-42a43ca60cb3',
+];
+final List<String> rainaudioPaths = [
+  //rain
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FRain%2Fthunder-sounds-long-73277.mp3?alt=media&token=cd97da44-c991-4188-9243-e941f258072f',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FRain%2Fmystic-forest-ambient-23812.mp3?alt=media&token=deaaf4a4-eda1-48b4-b016-f5fd3d9ae58a',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FRain%2Finside-freezer-74749.mp3?alt=media&token=8b123654-6a72-4c81-a9d9-dc5b1fb45d7c',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FRain%2Fambience-crickets-chirping-light-rain-tending-to-heavier-rain-10576.mp3?alt=media&token=394f064f-5a8d-4ecf-9a29-23f041786f02',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Homepage%2FRecommended%2FCity%20Rain.mp3?alt=media&token=84af8900-faa1-4ca4-931b-81499127a47c',
+];
+
+final List<String> musicaudioPaths = [
+  //music
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FMusic%2Flibrary-73743.mp3?alt=media&token=b7ebe94a-aeda-4fff-b1cc-37026fbe4058',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FMusic%2Fcoffee-shop-ambience-27829.mp3?alt=media&token=dd519139-43f3-4520-9f21-7d785487a4fc',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FMusic%2Fpeaceful-piano-for-focus-and-study-197336.mp3?alt=media&token=b35d17d7-d85e-4bdf-8c1d-161eefdaec15',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FMusic%2Fwaves-white-noise-9777.mp3?alt=media&token=96dccac1-a9af-493a-90a2-7a5af10a85d7',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FMusic%2Fambient-classical-guitar-144998.mp3?alt=media&token=c35febd8-0c40-4edb-9770-9d1b3824efd8',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FMusic%2Flost-ambient-lofi-60s-10821.mp3?alt=media&token=1369645b-3477-4389-86ff-7d0ba82e13b5',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FMusic%2Fpeaceful-piano-loop-6903.mp3?alt=media&token=86f9efed-e1d2-4753-9610-2bac898c31c1',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FMusic%2Fspirit-blossom-15285.mp3?alt=media&token=dc7706e8-7b30-4b94-b351-b93473ee14ea',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FMusic%2Fambient-classical-guitar-144998.mp3?alt=media&token=c35febd8-0c40-4edb-9770-9d1b3824efd8',
+];
+
+final List<String> asmraudioPaths = [
+  //ASMR
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FASMR%2Fwind-2-6196.mp3?alt=media&token=0d4eca9a-2c74-4dbf-836d-53b1e03bfae6',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FASMR%2F024340_asmr-tapping-69513.mp3?alt=media&token=c65f9540-e672-4868-a111-72a6389a38da',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FASMR%2Fbrushing-teeth-19787.mp3?alt=media&token=363afa53-5b1f-4f94-b62e-82d62e2df753',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FASMR%2Fpaper_unfolding_refolding-6425.mp3?alt=media&token=3ccfeb33-88d1-420c-b3cd-b47cfa0dd743',
+];
+final List<String> ambientaudioPaths = [
+  //ambient
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FAmbient%2Fgentle-ocean-waves-fizzing-bubbles-64980.mp3?alt=media&token=bb34df48-733a-4796-8905-4e1b7ee0050b',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FAmbient%2Fwind-blowing-sfx-12809.mp3?alt=media&token=3b8c8519-2286-4c2a-9f82-71f995bae10f',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FAmbient%2Frainforest-33441.mp3?alt=media&token=1390c832-6e01-4abb-9809-d1baff70d72b',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FAmbient%2Fbonfire-hq-6991.mp3?alt=media&token=7bd96590-d0c8-4333-88e1-f65803771887',
+];
+final List<String> productivityaudioPaths = [
+  //transport
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FProductivity%2Foffice-ambience-6322.mp3?alt=media&token=7fdf16de-8f91-41e4-bd23-5b91156d59f8',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FProductivity%2Fcomputer-keyboard-typing-snippet-mono-loop-31523.mp3?alt=media&token=58c4b089-4d17-4019-b9e2-90a92180103f',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FProductivity%2Fmouse-clicks-and-scrolling-54931.mp3?alt=media&token=5bcf6c9a-4a80-4a92-a2ec-afd9bc7b56eb',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FFocus%2FProductivity%2Fstudy-in-a-minor-113931.mp3?alt=media&token=5c679295-8732-4442-a22c-6ee6204c1710',
+];
+
+// final List<String> natureaudioNames = [
+//   //nature
+//   'Zen Garden',
+//   'Bamboo Forest',
+//   'Volcano Vista',
+//   'River Rapids',
+// ];
+
+// final List<String> rainaudioNames = [
+//   //rain
+//   'Thunderstorm',
+//   'Misty Morning',
+//   'Frozen Rainfall',
+//   'Tropical Thunder',
+//   'Cityscape Shower',
+// ];
+
+// final List<String> musicaudioNames = [
+//   //music
+//   'Library ambient',
+//   'Coffee Shop',
+//   'Focus',
+//   'White Noise',
+//   'Classical Study',
+//   'Instrumental Beats',
+//   'Ambient Piano',
+//   'Chillhop Mix',
+//   'Acoustic Guitar',
+// ];
+
+// final List<String> asmraudioNames = [
+//   //ASMR
+//   'Whispering Wind',
+//   'Gentle Tapping',
+//   'Soft Brushing',
+//   'Paper Crinkles',
+// ];
+
+// final List<String> ambientaudioNames = [
+//   //Ambient sounds
+//   'Calm Waves',
+//   'Mountain Breeze',
+//   'Rainforest Canopy',
+//   'Crackling Fireplace',
+// ];
+
+// final List<String> productivityaudioNames = [
+//   //Productivity sounds
+//   'Pomodoro Timer',
+//   'Keyboard Typing',
+//   'Pen Clicking',
+//   'Productivity Boost',
+// ];
+
+final List<String> natureaudioNames = [
+  //nature
+  'Zen',
+  'Bamboo',
+  'Volcano',
+  'River',
+  'Forest',
+];
+
+final List<String> rainaudioNames = [
+  //rain
+  'Thunder',
+  'Mist',
+  'Frozen',
+  'Tropical',
+  'City',
+];
+
+final List<String> musicaudioNames = [
+  //music
+  'Library',
+  'Coffee',
+  'Focus',
+  'White',
+  'Classical',
+  'Beats',
+  'Piano',
+  'Chillhop',
+  'Guitar',
+];
+
+final List<String> asmraudioNames = [
+  //ASMR
+  'Wind',
+  'Tap',
+  'Brush',
+  'Paper',
+];
+
+final List<String> ambientaudioNames = [
+  //Ambient sounds
+  'Waves',
+  'Breeze',
+  'Rainforest',
+  'Fireplace',
+];
+
+final List<String> productivityaudioNames = [
+  //Productivity sounds
+  'Office',
+  'Typing',
+  'Clicking',
+  'Boost',
+];
+
+final List<Widget> natureIcons = [
+  Image.asset('assets/focusIcons/nature/zen.png'),
+  Image.asset('assets/focusIcons/nature/bamboo.png'),
+  Image.asset('assets/focusIcons/nature/volcano.png'),
+  Image.asset('assets/focusIcons/nature/river.png'),
+  Image.asset('assets/focusIcons/nature/forest.png'),
+];
+
+final List<Widget> rainIcons = [
+  Image.asset('assets/focusIcons/rain/thunder.png'),
+  Image.asset('assets/focusIcons/rain/mist.png'),
+  Image.asset('assets/focusIcons/rain/frozen.png'),
+  Image.asset('assets/focusIcons/rain/tropical.png'),
+  Image.asset('assets/focusIcons/rain/city.png'),
+];
+final List<Widget> musicIcons = [
+  Image.asset('assets/focusIcons/music/library.png'),
+  Image.asset('assets/focusIcons/music/coffee.png'),
+  Image.asset('assets/focusIcons/music/focus.png'),
+  Image.asset('assets/focusIcons/music/white.png'),
+  Image.asset('assets/focusIcons/music/classical.png'),
+  Image.asset('assets/focusIcons/music/beats.png'),
+  Image.asset('assets/focusIcons/music/piano.png'),
+  Image.asset('assets/focusIcons/music/chillhop.png'),
+  Image.asset('assets/focusIcons/music/guitar.png'),
+];
+final List<Widget> asmrIcons = [
+  Image.asset('assets/focusIcons/asmr/wind.png'),
+  Image.asset('assets/focusIcons/asmr/tap.png'),
+  Image.asset('assets/focusIcons/asmr/brush.png'),
+  Image.asset('assets/focusIcons/asmr/paper.png'),
+];
+final List<Widget> ambientIcons = [
+  Image.asset('assets/focusIcons/ambient/waves.png'),
+  Image.asset('assets/focusIcons/ambient/breeze.png'),
+  Image.asset('assets/focusIcons/ambient/rainforest.png'),
+  Image.asset('assets/focusIcons/ambient/fireplace.png'),
+];
+final List<Widget> productivityIcons = [
+  Image.asset('assets/focusIcons/productivity/pomodoro.png'),
+  Image.asset('assets/focusIcons/productivity/typing.png'),
+  Image.asset('assets/focusIcons/productivity/clicking.png'),
+  Image.asset('assets/focusIcons/productivity/boost.png'),
+];
---
+++ composer/lib/data/composer_data/relaxing_composer.dart
@@ -0,0 +1,384 @@
+// import 'package:flutter/material.dart';
+
+// final List<String> natureaudioPaths = [
+//   //nature
+//   'assets/nature/cave.ogg',
+//   'assets/nature/creek.ogg',
+//   'assets/nature/desert.ogg',
+//   'assets/nature/fire.ogg',
+//   'assets/nature/lake.ogg',
+//   'assets/nature/night.ogg',
+//   'assets/nature/ocean.ogg',
+//   'assets/nature/rainforest.ogg',
+//   'assets/nature/wind.ogg',
+// ];
+
+// final List<String> animalsaudioPaths = [
+//   //Animals
+//   'assets/animals/birds.ogg',
+//   'assets/animals/rainforest_birds.ogg',
+//   'assets/animals/crickets.ogg',
+//   'assets/animals/frogs.ogg',
+//   'assets/animals/owls.ogg',
+//   'assets/animals/whales.ogg',
+//   'assets/animals/wolves.ogg',
+// ];
+// final List<String> rainaudioPaths = [
+//   //rain
+//   'assets/rain/rain.ogg',
+//   'assets/rain/thunders.ogg',
+//   'assets/rain/thunderstorm.ogg',
+//   'assets/rain/rain_in_forest.ogg',
+//   'assets/rain/rain_on_leaves.ogg',
+//   'assets/rain/rain_on_roof.mp3',
+//   'assets/rain/rain_on_tent.mp3',
+//   'assets/rain/rain_on_window.mp3',
+//   'assets/rain/rain_under_umbrella.ogg',
+// ];
+// final List<String> musicaudioPaths = [
+//   //music
+//   'assets/music/harp.mp3',
+//   'assets/music/piano.ogg',
+//   'assets/music/piano_2.ogg',
+//   'assets/music/guitar.mp3',
+//   'assets/music/lofi_guitar.mp3',
+//   'assets/music/guitar_sentimental.mp3',
+//   'assets/music/acoustic_guitar.mp3',
+//   'assets/music/violin.mp3',
+//   'assets/music/peaceful.mp3',
+//   'assets/music/rhodes.mp3',
+//   'assets/music/ambience.mp3',
+//   'assets/music/ambient.mp3',
+//   'assets/music/chill.mp3',
+//   'assets/music/cinematic.mp3',
+// ];
+// final List<String> asmraudioPaths = [
+//   //ASMR
+//   'assets/asmr/breathing.ogg',
+//   'assets/asmr/car_engine.ogg',
+//   'assets/asmr/cat_purring.ogg',
+//   'assets/asmr/chewing.ogg',
+//   'assets/asmr/crackling.ogg',
+//   'assets/asmr/hair_clippers.ogg',
+//   'assets/asmr/page_turning.ogg',
+//   'assets/asmr/scratching.ogg',
+//   'assets/asmr/tapping.ogg',
+//   'assets/asmr/whispering.ogg',
+// ];
+// final List<String> transportaudioPaths = [
+//   //transport
+//   'assets/transport/car.ogg',
+//   'assets/transport/train.ogg',
+//   'assets/transport/airplane.ogg',
+//   'assets/transport/boat.ogg',
+//   'assets/transport/boat_ride.ogg',
+// ];
+
+// final List<String> natureaudioNames = [
+//   //nature
+//   'Cave',
+//   'Creek',
+//   'Desert',
+//   'Fire',
+//   'Lake',
+//   'Night',
+//   'Ocean',
+//   'Forest',
+//   'Wind',
+// ];
+// final List<String> animalsaudioNames = [
+//   //animals
+//   "Birds",
+//   "Birds",
+//   "Crickets",
+//   "Frogs",
+//   "Owls",
+//   "Whales",
+//   "Wolves",
+// ];
+// final List<String> rainaudioNames = [
+//   //rain
+//   "Rain",
+//   "Thunder",
+//   "Thunder",
+//   "Forest",
+//   "Leaves",
+//   "Roof",
+//   "Tent",
+//   "Window",
+//   "Umbrella",
+// ];
+// final List<String> musicaudioNames = [
+//   //music
+//   'Harp',
+//   'Piano',
+//   'Piano',
+//   'Guitar',
+//   'Guitar',
+//   'Guitar',
+//   'Guitar',
+//   'Violin',
+//   'Peaceful',
+//   'Rhodes',
+//   'Ambient',
+//   'Ambient',
+//   'Chill',
+//   'Cinematic',
+// ];
+// final List<String> asmraudioNames = [
+//   //ASMR
+//   'Breathing',
+//   'Engine',
+//   'Cat Purring',
+//   'Chewing',
+//   'Crackling',
+//   'Hair Clip.',
+//   'Page',
+//   'Scratch',
+//   'Tapping',
+//   'Whispering',
+// ];
+// final List<String> transportaudioNames = [
+//   //transport
+//   'Car',
+//   'Train',
+//   'Airplane',
+//   'Boat',
+//   'Boat',
+// ];
+
+// final List<Widget> natureIcons = [
+//   Image.asset('assets/icons/cave.png'),
+//   Image.asset('assets/icons/creek.png'),
+//   Image.asset('assets/icons/desert.png'),
+//   Image.asset('assets/icons/fire.png'),
+//   Image.asset('assets/icons/lake.png'),
+//   Image.asset('assets/icons/night.png'),
+//   Image.asset('assets/icons/ocean.png'),
+//   Image.asset('assets/icons/forest.png'),
+//   Image.asset('assets/icons/wind.png'),
+// ];
+// final List<Widget> animalsIcons = [
+//   //animals
+//   Image.asset('assets/icons/birds.png'),
+//   Image.asset('assets/icons/birds2.png'),
+//   Image.asset('assets/icons/crickets.png'),
+//   Image.asset('assets/icons/frog.png'),
+//   Image.asset('assets/icons/owls.png'),
+//   Image.asset('assets/icons/whale.png'),
+//   Image.asset('assets/icons/wolves.png'),
+// ];
+// final List<Widget> rainIcons = [
+//   //rain
+//   Image.asset('assets/icons/rain/rain.png'),
+//   Image.asset('assets/icons/rain/storm.png'),
+//   Image.asset('assets/icons/rain/thunderstorm.png'),
+//   Image.asset('assets/icons/rain/rain_in_forest.png'),
+//   Image.asset('assets/icons/rain/rain_on_leaves.png'),
+//   Image.asset('assets/icons/rain/rain_on_roof.png'),
+//   Image.asset('assets/icons/rain/rain_on_leaves.png'),
+//   Image.asset('assets/icons/rain/rain_on_window.png'),
+//   Image.asset('assets/icons/rain/rain_under_umbrella.png'),
+// ];
+// final List<Widget> musicIcons = [
+//   //music
+//   Image.asset('assets/icons/harp.png'),
+//   Image.asset('assets/icons/piano.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+//   Image.asset('assets/icons/piano2.png'),
+// ];
+// final List<Widget> asmrIcons = [
+//   //ASMR
+//   Image.asset('assets/icons/asmr/breathing.png'),
+//   Image.asset('assets/icons/asmr/car_engine.png'),
+//   Image.asset('assets/icons/asmr/cat_purring.png'),
+//   Image.asset('assets/icons/asmr/chewing.png'),
+//   Image.asset('assets/icons/asmr/crackling.png'),
+//   Image.asset('assets/icons/asmr/hair_clippers.png'),
+//   Image.asset('assets/icons/asmr/page_turning.png'),
+//   Image.asset('assets/icons/asmr/scratching.png'),
+//   Image.asset('assets/icons/asmr/tapping.png'),
+//   Image.asset('assets/icons/asmr/whispering.png'),
+// ];
+// final List<Widget> transportIcons = [
+//   //transport
+//   Image.asset('assets/icons/transport/car.png'),
+//   Image.asset('assets/icons/transport/train.png'),
+//   Image.asset('assets/icons/transport/plane.png'),
+//   Image.asset('assets/icons/transport/boat.png'),
+//   Image.asset('assets/icons/transport/boat.png'),
+// ];
+
+import 'package:flutter/material.dart';
+
+final List<String> natureaudioPaths = [
+  //nature
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FNature%2Fcave.ogg?alt=media&token=fcf441b2-357c-436d-86fd-afd8c52b0e8a',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FNature%2Ffire.ogg?alt=media&token=d0f77e53-067f-46fa-bdbc-42ef4bfcfae4',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FNature%2Flake.ogg?alt=media&token=810207ec-c7f5-48a0-8c11-f118f3820562',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FNature%2Fnight.ogg?alt=media&token=c7ebc514-225c-4704-a0fa-84b27c90b956',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FNature%2Focean.ogg?alt=media&token=9ce6adb6-7106-4b89-8b90-d848dedd8fdf',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FNature%2Fwind.ogg?alt=media&token=7630e560-ca96-4d24-bce6-8e9b7b02eba0',
+];
+
+final List<String> animalsaudioPaths = [
+  //Animals
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FAnimals%2Fbirds.ogg?alt=media&token=dd34774d-1d0a-4334-aaac-c0c8dcb60cd7',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FAnimals%2Fcrickets.ogg?alt=media&token=3da94606-1e08-47c0-b1c9-f0f17d16c32c',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FAnimals%2Ffrogs.ogg?alt=media&token=7bd3e2a1-d922-4375-b230-32888d5d49f5',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FAnimals%2Fowls.ogg?alt=media&token=dba4f8ef-067f-4b25-a021-1e5a38532b5f',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FAnimals%2Fwolves.ogg?alt=media&token=d028e22d-e041-4c90-a77e-1ec038381cf7',
+];
+final List<String> rainaudioPaths = [
+  //rain
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FRain%2Frain.ogg?alt=media&token=7abbdcb3-f992-44a8-8e0c-e10e4f3d07b5',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FRain%2Fthunders.ogg?alt=media&token=edfe9876-fb93-4bf8-86c6-d958d4208827',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FRain%2Frain_in_forest.ogg?alt=media&token=3a1004aa-3f3e-436b-8be6-fcb6a16a59fe',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FRain%2Frain_on_leaves.ogg?alt=media&token=1a80a1c6-2eeb-4cb9-87cb-d0cd4f7453a6',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FRain%2Frain_on_tent.mp3?alt=media&token=63929112-bde5-4248-831a-176afea378ac',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FRain%2Frain_on_window.mp3?alt=media&token=237ec971-e93b-45f8-9eca-5d7502fcaa31',
+];
+final List<String> musicaudioPaths = [
+  //music
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FMusic%2Fharp.mp3?alt=media&token=65af37ab-0c42-42f7-822b-8f22876d5773',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FMusic%2Fpiano.mp3?alt=media&token=ef417a74-31b3-4800-8fe7-909548b7f2c8',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FMusic%2Fguitar.mp3?alt=media&token=dd1ce76b-c1e5-41f5-8d58-5d64cad8a4ae',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FMusic%2Fviolin.mp3?alt=media&token=4ac28fc2-effa-4a7b-9b6b-2638801ffc24',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FMusic%2Fflute.mp3?alt=media&token=45175da0-a601-4410-a0a7-b7e5135afd31',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FMusic%2Fkoto.mp3?alt=media&token=e40040b7-b4b0-45d4-b9ce-1a68c4eea42e',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FMusic%2Fsitar.mp3?alt=media&token=16be66c4-9c52-427d-bfec-d95fc4f88139',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FMusic%2Fbowl.mp3?alt=media&token=b20c02ee-b08b-4335-8a74-cd25952b1ca4',
+];
+final List<String> asmraudioPaths = [
+  //ASMR
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FASMR%2Fbreathing.ogg?alt=media&token=6db65a07-5fb7-4543-9fc2-e12700468e6a',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FASMR%2Fcat_purring.ogg?alt=media&token=8faf7157-a112-4f65-8138-1703adf28169',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FASMR%2Fcrackling.ogg?alt=media&token=11f6d1a1-a027-4cf5-bddf-48e8be039cdb',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FASMR%2Fhair_clippers.ogg?alt=media&token=ea27017b-a5c4-448d-9acd-fae9a9332170',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FASMR%2Fpage_turning.ogg?alt=media&token=c4ffba21-ff81-4c5f-a9f3-3b7ff5e712b5',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FASMR%2Fscratching.ogg?alt=media&token=1756cb2e-f35b-4778-b78f-8f7fcb52f03e',
+];
+final List<String> transportaudioPaths = [
+  //transport
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FTransport%2Fcar.ogg?alt=media&token=5f763636-c194-4cdf-a4fa-2b64c74db69d',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FTransport%2Ftrain.ogg?alt=media&token=61643252-06c6-469a-9a15-1fc62ccd6b56',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FTransport%2Fairplane.ogg?alt=media&token=02c004ed-dc9a-4e13-8ed3-1a5de9c6b04a',
+  'https://firebasestorage.googleapis.com/v0/b/aura-xd.appspot.com/o/Composers%2FRelaxing%2FTransport%2Fboat.ogg?alt=media&token=41370df1-a427-4bc3-936c-9209ef20c359',
+];
+
+final List<String> natureaudioNames = [
+  //nature
+  'Cave',
+  'Fire',
+  'Lake',
+  'Night',
+  'Ocean',
+  'Wind',
+];
+final List<String> animalsaudioNames = [
+  //animals
+  "Birds",
+  "Crickets",
+  "Frogs",
+  "Owls",
+  "Wolves",
+];
+final List<String> rainaudioNames = [
+  //rain
+  "Rain",
+  "Thunder",
+  "Forest",
+  "Leaves",
+  "Roof",
+  "Window",
+];
+final List<String> musicaudioNames = [
+  //music
+  'Harp',
+  'Piano',
+  'Guitar',
+  'Violin',
+  'Flute',
+  'Koto',
+  'Sitar',
+  'Bowl',
+];
+final List<String> asmraudioNames = [
+  //ASMR
+  'Breathing',
+  'Cat Purring',
+  'Bonfire',
+  'Hair Clip.',
+  'Page',
+  'Scratch',
+];
+final List<String> transportaudioNames = [
+  //transport
+  'Car',
+  'Train',
+  'Airplane',
+  'Boat',
+];
+
+final List<Widget> natureIcons = [
+  Image.asset('assets/icons/nature/cave.png'),
+  Image.asset('assets/icons/nature/fire.png'),
+  Image.asset('assets/icons/nature/lake.png'),
+  Image.asset('assets/icons/nature/night.png'),
+  Image.asset('assets/icons/nature/ocean.png'),
+  Image.asset('assets/icons/nature/wind.png'),
+];
+final List<Widget> animalsIcons = [
+  //animals
+  Image.asset('assets/icons/animals/birds.png'),
+  Image.asset('assets/icons/animals/crickets.png'),
+  Image.asset('assets/icons/animals/frogs.png'),
+  Image.asset('assets/icons/animals/owls.png'),
+  Image.asset('assets/icons/animals/wolves.png'),
+];
+final List<Widget> rainIcons = [
+  //rain
+  Image.asset('assets/icons/rain/rain.png'),
+  Image.asset('assets/icons/rain/storm.png'),
+  Image.asset('assets/icons/rain/rain_in_forest.png'),
+  Image.asset('assets/icons/rain/rain_on_leaves.png'),
+  Image.asset('assets/icons/rain/rain_on_roof.png'),
+  Image.asset('assets/icons/rain/rain_on_window.png'),
+];
+final List<Widget> musicIcons = [
+  //music
+  Image.asset('assets/icons/music/harp.png'),
+  Image.asset('assets/icons/music/piano.png'),
+  Image.asset('assets/icons/music/guitar.png'),
+  Image.asset('assets/icons/music/violin.png'),
+  Image.asset('assets/icons/music/flute.png'),
+  Image.asset('assets/icons/music/koto.png'),
+  Image.asset('assets/icons/music/sitar.png'),
+  Image.asset('assets/icons/music/bowl.png'),
+];
+final List<Widget> asmrIcons = [
+  //ASMR
+  Image.asset('assets/icons/asmr/breathing.png'),
+  Image.asset('assets/icons/asmr/cat_purring.png'),
+  Image.asset('assets/icons/asmr/crackling.png'),
+  Image.asset('assets/icons/asmr/hair_clippers.png'),
+  Image.asset('assets/icons/asmr/page_turning.png'),
+  Image.asset('assets/icons/asmr/scratching.png'),
+];
+final List<Widget> transportIcons = [
+  //transport
+  Image.asset('assets/icons/transport/car.png'),
+  Image.asset('assets/icons/transport/train.png'),
+  Image.asset('assets/icons/transport/plane.png'),
+  Image.asset('assets/icons/transport/boat.png'),
+];
